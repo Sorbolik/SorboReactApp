7c555cd02ac059371ed6d4764bb5f19aae5e770c
Sorbellos <48828353+Sorbellos@users.noreply.github.com>
2022-03-20 14:33:03 +0100

    first chat deploy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,84 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Button, Form, Input } from 'antd'
 import './App.css';
+import ChatPopUp from './components/ChatPopUp/ChatPopUp';
 
 function App() {
+
+  const ws = new WebSocket("ws://localhost:8080");
+
+  const formField = React.useRef(null)
+  const messagesEndRef = React.useRef(null)
+
+  const [messagesArray, setMessagesArray] = useState(
+    []
+  );
+
+  // const submitHandler = (e) => {
+  //   e.preventDefault();
+  //   submit();
+  // }
+
+  const submit = () => {
+    // let textNode = document.createElement('div');
+    // textNode.innerHTML = message;
+    // this.pageForm.controls['messageBox'].setValue('');
+    ws.send(formField?.current?.getFieldValue('message'));
+    formField?.current.setFields([{
+      name: 'message',
+      value: ''
+    }])
+    console.log(`We sent a message to the server ${formField?.current?.getFieldValue('message')}`)
+
+  }
+
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
+  }
+
+
+  useEffect(() => {
+    ws.onmessage = (message) => {
+      messagesArray.push(message.data);
+      setMessagesArray([...messagesArray]);
+      // let textNode1 = document.createElement('div');
+      // textNode1.innerHTML = message.data;
+      // document.getElementById('messages').append(textNode1);
+      console.log(`We received a message from the server ${message.data}`)
+      console.log(`MESSAGES ${messagesArray}`)
+
+    }
+  }, [])
+
+
+  useEffect(scrollToBottom, [messagesArray]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='popups'>
+          {messagesArray.map((elem, i) => {
+            return <ChatPopUp actualText={elem} index={i} key={i}></ChatPopUp>
+          })}
+          <div style={{ marginBottom: "39px" }} ref={messagesEndRef} />
+        </div>
+        <Form onFinish={submit} ref={formField}>
+          <Form.Item
+            name='message'
+            initialValue=''>
+            <Input></Input>
+          </Form.Item>
+          <Button onClick={submit}>INVIA</Button>
+
+          {/* <div className='inputBox'>
+            <label>
+              Scrivi:
+            <input type="text" name="message" />
+            </label>
+            <input onClick={submit} type="button" value="Invia" />
+          </div> */}
+        </Form>
       </header>
     </div>
   );
